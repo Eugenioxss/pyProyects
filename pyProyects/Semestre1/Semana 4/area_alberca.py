ef7dac2838abaf9500bddbffc23fa269829449ed
Eugenioxss <159830194+Eugenioxss@users.noreply.github.com>
2024-09-19 07:31:56 -0600

    Lo nuevopro

--- pyProyects/Semestre1/Semana 4/area_alberca.py
+++ pyProyects/Semestre1/Semana 4/area_alberca.py
@@ -1,12 +1,14 @@
 def area(base,altura,fondo):
     resultado=base*altura+2*(altura*fondo)+2*(fondo*base)
-    return resultado
+    print("holaaa")
+    #return resultado
 
 def main():
     base=float(input("Dame la base de la base de la alberca: "))
     altura=float(input("Dame el altura de la base de la alberca: "))
     profundidad=float(input("Dame la profunidad: "))
     medida=input("Que medida estas usando? ")
-    print(area(base,altura,profundidad), medida)
+    area(1,2,3)
+    #print(area(base,altura,profundidad), medida)
     
 main()
