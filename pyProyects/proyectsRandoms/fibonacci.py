b54dab8fa10953a89be443ba437a26735c033cb1
Commodore <moreugenio@gmail.com>
2025-03-31 17:12:32 -0600

    holA

--- pyProyects/proyectsRandoms/fibonacci.py
+++ pyProyects/proyectsRandoms/fibonacci.py
@@ -1,5 +1,13 @@
 def main():
+    fibolist=[1,1]
     numi=int(input("Dame un número: "))
-    for i in range (1,numi):
-        fiboant=i+i
+    if numi == 1:
+        print(fibolist.pop(-1))
+    elif numi == 2:
+        print(fibolist)
+    else:
+        for i in range (2,numi):
+            fibolist.append(fibolist[-1]+fibolist[-2])
+        print(fibolist)
+
 main()
